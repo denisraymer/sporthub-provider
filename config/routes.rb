b215434e325dbcd6ac6497d0907e601c97ca69eb
denisraymer <to.mars.66.miles@gmail.com>
2020-10-17 17:51:37 +0300

    Modules are installed bootstrap, jquery and popper.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'posts/index'
+  root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
