97fbbd51a61d2bd0c088f925f48fcf0671402b24
denisraymer <to.mars.66.miles@gmail.com>
2020-10-19 17:05:12 +0300

    Added page layout with posts and post viewing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   get 'posts/index'
+  get 'post/:id', to: 'posts#show'
+
   root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
