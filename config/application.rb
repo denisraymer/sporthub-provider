f31169ff1071336f245538121516e045e56db607
denisraymer <to.mars.66.miles@gmail.com>
2020-10-22 00:10:45 +0300

    Exclusive fonts are enabled

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SportHub
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
