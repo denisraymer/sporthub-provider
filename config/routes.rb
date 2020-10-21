46a9e0093f2d0e45e6053fe80afbdb7c0914f6c3
denisraymer <to.mars.66.miles@gmail.com>
2020-10-21 23:02:37 +0300

    Created splash_screen controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'splash_screen/index'
   get 'posts/index'
   get 'post/:id', to: 'posts#show'
 
-  root to: 'posts#index'
+  root to: 'splash_screen#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
