3b0aa4b97700653406cb92f3c37bb5ff3997ad55
denisraymer <to.mars.66.miles@gmail.com>
2020-10-25 20:10:55 +0300

    Added new routes for the program page and the main page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 Rails.application.routes.draw do
   get 'splash-screen', to: 'splash_screen#index'
-  get 'posts/index'
-  get 'post/:id', to: 'posts#show'
+  get 'programs', to: 'posts#index'
+  get 'program/:id', to: 'posts#show'
 
   root to: 'splash_screen#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
