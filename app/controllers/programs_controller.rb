8ea7346164b1fd56813ab4164b01da84af8416cd
denisraymer <to.mars.66.miles@gmail.com>
2020-10-29 01:48:32 +0300

    The Programs controller was created and everything from the Posts controller was moved to it

---
+++ app/controllers/programs_controller.rb
@@ -0,0 +1,7 @@
+class ProgramsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 Rails.application.routes.draw do
   get 'splash-screen', to: 'splash_screen#index'
-  get 'programs', to: 'posts#index'
-  get 'program/:id', to: 'posts#show'
+  get 'programs', to: 'programs#index'
+  get 'program/:id', to: 'programs#show'
 
   root to: 'splash_screen#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
