d4dba0a88fec6f80d2108efa7f47f13e543f49e5
denisraymer <to.mars.66.miles@gmail.com>
2020-10-29 01:24:19 +0300

    Callbacks in models

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,4 +1,11 @@
 class Program < ApplicationRecord
   validates :progress, numericality: {greater_than: 0, allow_nil: true}
   validates :name, :author, :category, presence: true
+
+  after_initialize {p 'initialize'} # Program.new Program.first
+  after_save {p 'save'} # Program.create Program.save
+  after_create {p 'create'} # Program.create
+  after_update {p 'update'} # Program.update
+  after_destroy {p 'destroy'} # Program.last.destroy
+
 end
