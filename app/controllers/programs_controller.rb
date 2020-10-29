11369a4bb76f0588f7cb7def9e580730a7513fbd
denisraymer <to.mars.66.miles@gmail.com>
2020-10-29 03:54:41 +0300

    Getting data and rendering via the controller
    * Rendering in View is overwritten

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,6 +1,8 @@
 class ProgramsController < ApplicationController
   def index
     @programs = Program.all
+
+    render body: @programs.map { |program| "#{program.name} | #{program.progress} | #{program.category}" }
   end
 
   def show
