629b3e7ea69d5dfcc91fd451156b4b2387e3409a
denisraymer <to.mars.66.miles@gmail.com>
2020-10-31 22:59:17 +0300

    Created a method for adding programs to the database

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,12 +1,23 @@
 class ProgramsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @programs = Program.all
   end
 
   def create
-    render body: 'created'
+    program = Program.create(programs_params)
+
+    if program.persisted?
+      render json: 'Created a program: ' + program.name, status: :created
+    else
+      render json: 'Error during creation: ' + program.errors, status: :unprocessable_entity
+    end
   end
 
-  def show
+  private
+
+  def programs_params
+    params.permit(:name, :progress, :author, :description, :private, :category)
   end
 end
